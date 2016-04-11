cc514cd3b907da59c922d15a4d9d726bb0c3527f
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-04-11 16:29:42 -0300

    binary search update

--- Fibonacci/fib.c
+++ Fibonacci/fib.c
@@ -1,26 +1,74 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <strings.h>
 
 //#define ARRAY_SIZE( array ) sizeof( array ) / sizeof( array[0] )
 
 int fibonacci(int value);
 void fillArray(int array[], int size);
+int binarySearch(int array[], int size, int value);
 
 int main(int argc, char const *argv[])
 {
 	int fibonacci_elements = 40;
-	int array[fibonacci_elements];
+	int fibo[fibonacci_elements];
 	int i = 0;
+	int value = -1;
 
-	fillArray(array, fibonacci_elements);
+	fillArray(fibo, fibonacci_elements);
 
 	for (i = 0; i < fibonacci_elements; i++)
 	{
-		printf("%d\n", array[i]);
+		printf("%d\n", fibo[i]);
+	}
+
+	printf("\n*\nEntre com o valor a ser pesquisado\n*\n");
+
+	while(value != 0)
+	{
+		scanf("%d", &value);
+
+		binarySearch(fibo, fibonacci_elements, value);
 	}
 
 	return 0;
+}
+
+int binarySearch(int array[], int size, int value)
+{
+	int start = 0;
+	int end   = size - 1;
+	int midle = 0;
+	int index = 0;
+	int found = 0;
+
+	while (!found)
+	{
+		midle = ((start + end) / 2);
+		
+		if (array[midle] == value)
+		{
+			index = midle;
+			found = 1;
+		}
+		else if (array[midle] < value)
+		{
+			start = midle + 1;
+		} 
+		else if (array[midle] > value)
+		{
+			end = midle - 1;
+		}
+
+		if (end > start)
+		{
+			printf("O valor %d nao se encontra no vetor\n", value);
+			found = 1;
+			index = -1;
+		}
+	}
+
+	return index;
 }
 
 int fibonacci(int value)
